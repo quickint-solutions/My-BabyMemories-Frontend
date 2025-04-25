82a2c065ca2ba85aa95c14ed007295b0a363afbb
koshti sagar <koshtisagar7778@gmail.com>
2025-04-25 18:22:28 +0530

    me api call

--- api-handler/auth/index.ts
+++ api-handler/auth/index.ts
@@ -41,7 +41,7 @@ export const signupHandler = async (data: signupData) => {
 export const fetchUserData = async (token: string) => {
   try {
     const response = await axios.get(
-      `http://localhost:3000/api/auth/me`,
+      `http://localhost:3000/api/user/me`,
       {
         headers: {
           Authorization: `Bearer ${token}`,
